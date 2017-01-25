c7de616e7dcc70c6c5973a166c672a0e31b73ccf
Marie Treschow <treschow.marie@gmail.com>
2017-01-25 12:34:24 +0100

    setting up project

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const express = require('express');
+
+const express = require('express'),
+      hbs = require('hbs'),
+      bodyParser = require('body-parser'),
+      displayRoutes = require('express-routemap')
+      morgan = require('morgan');
+
+var app = express(),
+     db = require('./models');
+
+
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+   res.render('index');
+});
+
+
+db.sequelize.sync({}).then(() => {
+   app.listen(3000, (req, res) => {
+      displayRoutes(app);
+      console.log('App listening on 3000!');
+   });
+});
