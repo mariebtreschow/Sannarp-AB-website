4d2030007d0e3e07bd6631744bca118ac981cecd
Marie Treschow <treschow.marie@gmail.com>
2017-02-03 16:30:12 +0100

    setting up sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,3 @@ const hbs = require('hbs'),
       uglify = require('gulp-uglify'),
       size = require('gulp-size'),
       concat = require('gulp-concat');
-
-gulp.task('scss', () => {
-   gulp.src('frontend/scss/application.scss')
-   .pipe(sourcemaps.init())
-   .pipe(sass({ outputStyle: 'compressed'}))
-   .pipe(concat('application.scss'))
-   .pipe(size())
-   .pipe(sourcemaps.write())
-   .pipe(gulp.dest('public/css'));
-});
-
-gulp.task('default', [ 'scss', 'js' ], () => {
-   gulp.watch('frontend/scss/**/*.scss', ['scss']);
-//   gulp.watch('frontend/js/**/*.js', ['js']);
-});
