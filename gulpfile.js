8900343ff1cb48de62d2d27556dc4d1675d8c183
Marie Treschow <treschow.marie@gmail.com>
2017-02-07 10:52:26 +0100

    updating sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 const hbs = require('hbs'),
       gulp = require('gulp'),
-      sass = require('gulp-sass'),
+      //sass = require('gulp-sass'),
       sourcemaps = require('gulp-sourcemaps'),
       uglify = require('gulp-uglify'),
       size = require('gulp-size'),
       concat = require('gulp-concat');
+
+
+var customizeBootstrap = require('gulp-customize-bootstrap');
+var sass = require('gulp-sass');
+
+gulp.task('compileBootstrap', function() {
+   return gulp.src('node_modules/bootstrap/scss/bootstrap.scss')
+      .pipe(customizeBootstrap('styles/scss/*.scss'))
+      .pipe(sass())
+      .pipe(gulp.dest('styles/'));
+});
